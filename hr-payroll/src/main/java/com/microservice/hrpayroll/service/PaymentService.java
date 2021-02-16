e38ca15879b59a7865f827db56a77a235badec08
Thiago Henrique Ferreira <thiagohferreira10@gmail.com>
2021-02-16 15:23:10 -0300

    utilizando feign client

--- hr-payroll/src/main/java/com/microservice/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/service/PaymentService.java
@@ -1,13 +1,20 @@
 package com.microservice.hrpayroll.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.microservice.hrpayroll.WorkerFeignClient;
 import com.microservice.hrpayroll.entities.Payment;
+import com.microservice.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
-	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Thiago", 30.0, days);
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	public Payment getPayment(long workerId, Integer days) {
+		Worker worker = workerFeignClient.getById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
