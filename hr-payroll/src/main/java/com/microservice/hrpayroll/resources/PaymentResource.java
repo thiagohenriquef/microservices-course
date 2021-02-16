e76e7381a5c9cf2c9745ddd7ed97d36fd675a6c4
Thiago Henrique Ferreira <thiagohferreira10@gmail.com>
2021-02-16 20:48:50 -0300

    configurando api gateway zuul e fallback com hystrix

--- hr-payroll/src/main/java/com/microservice/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/resources/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.hrpayroll.entities.Payment;
 import com.microservice.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payment")
@@ -17,8 +18,13 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		return ResponseEntity.ok(service.getPayment(workerId, days));
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(@PathVariable Long workerId, Integer days) {
+		return ResponseEntity.ok(new Payment("Thiago", 400.0, days));
 	}
 }
