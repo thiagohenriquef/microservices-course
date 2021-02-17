3d51768d66b920bb2fdd0d026733b6ab3b33d956
Thiago Henrique Ferreira <thiagohferreira10@gmail.com>
2021-02-17 20:16:59 -0300

    endpoints de usuário

---
+++ hr-user/src/main/java/com/microservice/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.microservice.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByEmail(String email);
+}
