9ba19c318905134477ea960941d656a2fff4f982
Thiago Henrique Ferreira <thiagohferreira10@gmail.com>
2021-02-18 01:23:55 -0300

    parametrizando configurações no repositório de configuração

--- hr-oauth/src/main/java/com/microservice/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/microservice/hroauth/config/AppConfig.java
@@ -1,14 +1,20 @@
 package com.microservice.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecretKey;
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -17,7 +23,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-		accessTokenConverter.setSigningKey("MY_SECRET_KEY");
+		accessTokenConverter.setSigningKey(jwtSecretKey);
 		return accessTokenConverter;
 	}
 
--- hr-oauth/src/main/java/com/microservice/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/microservice/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.microservice.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +18,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	private static final int SECONDS = 60 * 60 * 24;
+	
+	@Value("${oauth.client.name}")
+	private String oauthClientName;
+
+	@Value("${oauth.client.secret}")
+	private String oauthClientSecret;
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -41,8 +48,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients
 			.inMemory()
-			.withClient("myappname123")
-			.secret(passwordEncoder.encode("myappsecret123"))
+			.withClient(oauthClientName)
+			.secret(passwordEncoder.encode(oauthClientSecret))
 			.scopes("read", "write")
 			.authorizedGrantTypes("password")
 			.accessTokenValiditySeconds(SECONDS);
